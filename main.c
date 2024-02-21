166aed805ebe6aca993ea8d3401e64499347ad1b
hitesh788 <154747037+hitesh788@users.noreply.github.com>
2024-02-21 15:11:38 +0530

    Sum of array numbers

--- main.c
+++ main.c
@@ -1,27 +1,37 @@
-#include <stdio.h>
 
-int main(void) {
-  int n;
-  printf("Enter a number: ");
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int *arr;
+  int n, i;
+  int sum = 0;
+
+  printf("Enter the number of elements: ");
   scanf("%d", &n);
 
-  int *arr=(int*)malloc(n*sizeof(int));
-  printf("Enter the elements:");
-  for (int i=0; i<n; i++){
-    scanf("%d",&arr[i]);
+  // allocationg the memory for n numbers of array//
+
+  arr = (int *)malloc(n * sizeof(int));
+
+  // checking if the array is null and memory is allocated or not//
+  if (arr == NULL) {
+    printf("Memory not allocated.\n");
+    exit(0);
   }
-  printf("The odd element are:\n");
-  for(int i=0; i<n; i++){
-    if(arr[i]%2!= 0){
-      printf("%d\n", arr[i]);
-    
-    }
+  // taking input from user
+  printf("Enter the elements: ");
+  for (i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
   }
-  printf("The even element are:\n");
-  for(int i=0; i<n; i++){
-    if(arr[i]%2 == 0){
-      printf("%d\n", arr[i]);
-    }
+  // calculating the sum of element of array
+  for (i = 0; i < n; i++) {
+    sum += arr[i];
   }
+  // printing the sum of array
+  printf("The sum of array elements: %d\n", sum);
+
+  free(arr);
+
   return 0;
 }
