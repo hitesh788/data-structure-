7c252379f000e2973a40aaeb127c8667bcaac6f8
hitesh788 <154747037+hitesh788@users.noreply.github.com>
2024-02-21 14:42:58 +0530

    Odd and even in array

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
-    int n, *arr, i;
-    int sum_odd = 0, sum_even = 0;
+int main(void) {
+  int n;
+  printf("Enter a number: ");
+  scanf("%d", &n);
 
-    printf("Enter the size of the array: ");
-    scanf("%d", &n);
-
-    arr = (int*)malloc(n * sizeof(int));
-
-    printf("Enter the elements of the array:\n");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-        if (arr[i] % 2 == 0) {
-            sum_even += arr[i];
-        } else {
-            sum_odd += arr[i];
-        }
+  int *arr=(int*)malloc(n*sizeof(int));
+  printf("Enter the elements:");
+  for (int i=0; i<n; i++){
+    scanf("%d",&arr[i]);
+  }
+  printf("The odd element are:\n");
+  for(int i=0; i<n; i++){
+    if(arr[i]%2!= 0){
+      printf("%d\n", arr[i]);
+    
     }
-
-    printf("Sum of even elements: %d\n", sum_even);
-    printf("Sum of odd elements: %d\n", sum_odd);
-
-    free(arr);
-    return 0;
+  }
+  printf("The even element are:\n");
+  for(int i=0; i<n; i++){
+    if(arr[i]%2 == 0){
+      printf("%d\n", arr[i]);
+    }
+  }
+  return 0;
 }
